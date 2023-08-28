8729fd0113e233a0884c3a5c38cad5856b2fb68e
frumilly <ouerfelli.fzahra2@gmail.com>
2023-08-28 15:00:08 +0200

    avancement

---
+++ data/data_test.js
@@ -0,0 +1,31 @@
+
+        // Les données de recettes (similaires à celles que vous avez fournies)
+        const recipes = [
+            {
+                name: "Limonade à la noix de coco",
+                description: "Une délicieuse limonade exotique.",
+                ingredients: [
+                    { ingredient: "citron" },
+                    { ingredient: "noix de coco" }
+                ],
+                tags: ["boisson", "citron", "noix de coco"]
+            },
+            {
+                name: "Limonade à la cannelle",
+                description: "Une limonade parfumée à la cannelle.",
+                ingredients: [
+                    { ingredient: "citron" },
+                    { ingredient: "cannelle" }
+                ],
+                tags: ["boisson", "citron", "cannelle"]
+            },
+            {
+                name: "Poulet à la cannelle",
+                description: "Un plat de poulet délicieusement épicé.",
+                ingredients: [
+                    { ingredient: "poulet" },
+                    { ingredient: "cannelle" }
+                ],
+                tags: ["plat principal", "poulet", "cannelle"]
+            }
+        ];
